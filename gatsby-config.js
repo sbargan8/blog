503d26fd268fd67940161d52dadbd3fb2a4388d4
Tomek <46640406+tomekbotwicz@users.noreply.github.com>
2022-08-13 13:21:17 +0200

    cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ require("dotenv").config({
 
 module.exports = {
   siteMetadata: {
-    title: "Gatsby Contentful Starter",
-    description: "Official Contentful Gatsby Starter",
+    title: "Bargan Blog",
+    description: "Blog na temat filozofii!",
   },
   plugins: [
     "gatsby-transformer-sharp",
